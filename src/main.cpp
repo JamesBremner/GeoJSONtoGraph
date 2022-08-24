7531b6a106323c72d2b495e08a074b7fa26d4e25
JamesBremner <james@ravenspoint.com>
2022-08-24 15:12:38 -0400

    add code

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,124 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
+#include <set>
 
-class cGUI : public cStarterGUI
+typedef std::pair<double,double> vertex_t;
+
+class cEdge
 {
 public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
-    {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+    vertex_t v1, v2;
 
-        show();
-        run();
+    void display()
+    {
+        //std::cout << x1 << " " << y1 << ", " << x2 << " " << y2;
+    }
+};
+class cLineString
+{
+public:
+    std::vector<cEdge> myEdge;
+
+    void display()
+    {
+        for (auto &e : myEdge)
+        {
+            e.display();
+            std::cout << " => ";
+        }
+        std::cout << "\n";
+    }
+};
+class cSolution
+{
+public:
+    void generateSmallTest()
+    {
+        std::vector<std::string> input{
+            "{\"type\": \"Feature\", \"properties\": {\"id\": 1}, \"geometry\": { \"type\": \"LineString\", \"coordinates\": [ [ 147.0, -4.8 ], [ 141.0, -2.0 ] ]}}",
+            "{\"type\": \"Feature\", \"properties\": {\"id\": 2}, \"geometry\": { \"type\": \"LineString\", \"coordinates\": [ [ 152.6, -5.2 ], [ 152.05, -3.8 ], [ 147.0, -4.8 ] ] } }",
+            "{\"type\": \"Feature\", \"properties\": {\"id\": 3}, \"geometry\": { \"type\": \"LineString\", \"coordinates\": [ [ 147.0, -4.8 ], [ 144.73, 0.0 ] ] } }"};
+        myInput = input;
+    }
+    cLineString extractLocation(const std::string &line)
+    {
+        cLineString ret;
+        cEdge edge;
+        int p = line.find("[");
+        bool first = true;
+        while (true)
+        {
+            auto dbg = line.substr(p + 1);
+            p = line.find("[", p + 1);
+            if (p == -1)
+                break;
+            if (first)
+            {
+                first = false;
+                edge.v1.first = atof(line.substr(p + 1).c_str());
+                p = line.find(",", p + 1);
+                edge.v1.second = atof(line.substr(p + 1).c_str());
+                p = line.find("[", p + 1);
+            }
+            else
+            {
+                edge.v1 = edge.v2;
+            }
+            edge.v2.first = atof(line.substr(p + 1).c_str());
+            p = line.find(",", p + 1);
+            edge.v2.second = atof(line.substr(p + 1).c_str());
+            ret.myEdge.push_back(edge);
+        }
+        ret.display();
+        return ret;
+    }
+    void extractLocation()
+    {
+        for (auto &l : myInput)
+            myLineStringLocations.push_back(extractLocation(l));
+    }
+
+    void constructUniqueVertices()
+    {
+        // loop over all line string locations
+        for (auto &l : myLineStringLocations)
+        {
+            // loop over edges in string location
+            bool first = true;
+            for (auto &e : l.myEdge)
+            {
+                if (first)
+                {
+                    first = false;
+                    // save, dropping duplicates, the first vertex of the first edge
+                    myVertexSet.insert(e.v1);
+                }
+                // save, dropping duplicates, the second vertex
+                myVertexSet.insert(e.v2);
+            }
+        }
+
+        // display unique vertices found
+        std::cout << "unique vertices\n";
+        for( auto& v : myVertexSet )
+        {
+            std::cout << v.first <<" "<< v.second << "\n";
+        }
     }
 
 private:
-    wex::label &lb;
+    std::vector<std::string> myInput;
+    std::vector<cLineString> myLineStringLocations;
+    std::set<vertex_t> myVertexSet;
 };
 
 main()
 {
-    cGUI theGUI;
+    cSolution S;
+    S.generateSmallTest();
+    S.extractLocation();
+    S.constructUniqueVertices();
+
     return 0;
 }
